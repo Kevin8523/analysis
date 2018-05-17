56db512a80c7fbc391a99f6414d6d158ebb5a20f
Kevin Huang <Kevin8523@gmail.com>
2018-05-16 23:28:05 -0500

    update

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import os, sys
-filename = '/users/kevin8523/desktop/github/coffee_consulting/data/2014.csv'
+path = '/users/kevin8523/desktop/github/coffee_consulting/data/'
+filename = 'All.txt'
+filepath = f'{path}{filename}'
 # Note: This code was developed using Anaconda 4.5.3 with Python 3.6.5
 """
 # Notes / tricks learned from this project
@@ -9,10 +11,38 @@ filename = '/users/kevin8523/desktop/github/coffee_consulting/data/2014.csv'
 2) __file__ path of where the script is located
     a. sys.path.append(os.path.join(os.path.dirname('__file__'), "lib"))
     b. script_path = os.path.dirname(os.path.abspath( __file__ ))
+3) Cmd + I for help on objects
+4) #%% to create cells
 """
+#%% Load the data into a DataFrame
+df = pd.read_csv(filepath, sep='\t', header=0, encoding='latin-1')
 
-data = pd.read_csv(filename)
+print(df.shape)
+df.head(5)
+df.tail(5)
+df.info()
+df.describe()
+
+#%% Feature Engineering 
+df['Date']
 
 
+#RCODE
+#Create the Year/Month/Day
+data$Date <- str_replace_all(data$Date, "/", "-")
+data$Date <- mdy(data$Date)
+which(is.na(data$Date))
+data$Year <- year(data$Date)
+data$Month <- month(data$Date)
+data$Day <- day(data$Date)
+
+#Create Times
+data$Time <- hms(data$Time)
+which(is.na(data$Time))
+data$Hour <- hour(data$Time)
+data$Minute <- minute(data$Time)
+data$Second <- second(data$Time)
+
+data$`Time Zone` <- NULL
 
 
